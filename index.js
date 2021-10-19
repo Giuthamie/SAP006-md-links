8c8a37f6c022fbcb14c6544f291ea28450210032
giuthamie <giumie@hotmail.com>
2021-10-19 09:21:50 -0300

    function getFile

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = () => {
-  // ...
-};
+const chalk = require('chalk')
+const fs = require('fs')
+
+console.log(chalk.blue("bin"))
+///fs.readFile(file, [encoding], [callback]);
+function handleError(error) {
+    throw new Error(chalk.red(error.code, 'Não há arquivo no caminho'));
+}
+function getFile(filePath) {
+    const encoding = 'utf-8'
+    fs.readFile(filePath, encoding, (error, data) => {
+        if (error) {
+            handleError(error);
+        } console.log(chalk.green(data))
+    })
+}
+getFile('README.md')
